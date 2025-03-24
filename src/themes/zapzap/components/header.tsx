accbb3af30d39d2de3ae9fc26d348a38246c473d
JoaoVLima <celu.joaolima@gmail.com>
2025-03-24 16:26:30 -0300

    max-width header

--- src/themes/zapzap/components/header.tsx
+++ src/themes/zapzap/components/header.tsx
@@ -2,12 +2,12 @@ import React from "react";
 
 export function Header() {
     return (
-        <header className={'w-full select-none h-12 fixed top-0 px-4 py-2 text-black dark:text-white bg-white dark:bg-black'}>
+        <header className={'w-full max-w-md select-none h-12 fixed top-0 px-4 py-2 text-black dark:text-white bg-white dark:bg-black'}>
             <nav>
                 <ul className={'flex flex-row overflow-hidden truncate'}>
                     <li className={'basis-1/3 flex flex-row items-center justify-start'}>
                         <h1 className={'text-green-500 dark:text-white text-2xl bg-inherit'}>
-                            ZapZap
+                            Whatsapp
                         </h1>
                     </li>
                     <li className={'basis-2/3 flex flex-row items-center justify-end'}>
