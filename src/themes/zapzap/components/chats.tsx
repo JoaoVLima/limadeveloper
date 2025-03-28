aecf2e6b18c1b6dd8c7be401663c1772f07d80dc
JoaoVLima <celu.joaolima@gmail.com>
2025-03-28 14:00:10 -0300

    posts from json

--- src/themes/zapzap/components/chats.tsx
+++ src/themes/zapzap/components/chats.tsx
@@ -8,12 +8,12 @@ export function Chats() {
 
                 {postsData.posts.map((post) => (
                     <article className={'w-full px-4 py-2 h-16 flex flex-row gap-3 overflow-hidden truncate hover:bg-neutral-100 dark:hover:bg-neutral-900'}>
-                        <img className={'basis-1/12 rounded-full aspect-square'} src={'https://ui-avatars.com/api/?name=br&background=random'} alt={''}/>
+                        <img className={'basis-1/12 rounded-full aspect-square'} src={'https://ui-avatars.com/api/?background=random&name='+post.title} alt={''}/>
                         <section className={'basis-10/12 flex flex-col overflow-hidden truncate'}>
-                            <h4 className={'text-inherit'}>GitHub</h4>
-                            <p className={'text-neutral-500 truncate'}>Projeto atualizado</p>
+                            <h4 className={'text-inherit'}>{post.title}</h4>
+                            <p className={'text-neutral-500 truncate'}>{post.content}</p>
                         </section>
-                        <time className={'basis-1/12 text-neutral-500'} dateTime="2025-01-28 19:00">Ontem</time>
+                        <time className={'basis-1/12 text-neutral-500'} dateTime={post.timestamp}>{post.timestamp}</time>
                     </article>
                 ))}
 
