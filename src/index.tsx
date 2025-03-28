373526172312810c0ae750021adab4a600b7e8f4
JoaoVLima <celu.joaolima@gmail.com>
2025-03-28 14:16:17 -0300

    adding speed insights and removing old context

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import './style.css'
 import {Home} from './home.tsx';
-import {ThemeProvider} from './context.tsx';
 
 function init() {
     const home = document.getElementById('home') as HTMLDivElement;
     const root = createRoot(home);
     root.render(
         <StrictMode>
-            <ThemeProvider>
-                <Home/>
-            </ThemeProvider>
+            <Home/>
+            <SpeedInsights />
         </StrictMode>
     );
 }
