8f941a96edbfc94f4c224538dacbe7bb4c5e004f
JoaoVLima <celu.joaolima@gmail.com>
2025-09-09 20:23:57 -0300

    writting a better header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,19 @@
-import { useTranslation } from 'react-i18next'
+// import {useTranslation} from 'react-i18next'
 
 function Header() {
-    const { t } = useTranslation('landing_page')
+    // const {t} = useTranslation('landing_page')
 
-    return <>oi {t('HEADER')}</>
+    return (
+        <header className="sticky top-0 bg-black/50 z-50">
+            <nav className="mx-auto px-4">
+                <ul className="flex space-x-6 py-4 text-white font-medium">
+                    <li className="bg-neutral-700/50 py-1 px-4 rounded-md cursor-pointer">1(home)</li>
+                    <li className="bg-neutral-900/50 py-1 px-4 rounded-md cursor-pointer">2(something)</li>
+                    <li className="bg-neutral-900/50 py-1 px-4 rounded-md cursor-pointer">3(contact)</li>
+                </ul>
+            </nav>
+        </header>
+    )
 }
 
 export default Header
