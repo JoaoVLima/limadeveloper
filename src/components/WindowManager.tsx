09cf523b08ad5354f45f36dcc90d74d27b2a9cb6
JoaoVLima <celu.joaolima@gmail.com>
2025-09-12 15:22:24 -0300

    dinamic example with swapy

--- src/components/WindowManager.tsx
+++ src/components/WindowManager.tsx
@@ -1,20 +1,20 @@
-import {type JSX, useEffect, useMemo, useRef, useState} from 'react'
-import {type SlotItemMapArray, type Swapy, utils } from 'swapy'
+import { type ReactNode, useEffect, useMemo, useRef, useState } from 'react'
+import { type SlotItemMapArray, type Swapy, utils } from 'swapy'
 import { createSwapy } from 'swapy'
-import Window from "./Window.tsx";
 
 type Item = {
     id: string
     title: string
-    content?: JSX.Element;
+    content?: ReactNode
 }
 
 const initialItems: Item[] = [
-    { id: '1', title: '1', content: <div className="size-5 bg-amber-300"></div> },
-    { id: '2', title: '2', content: <div className="size-5 bg-amber-300"></div> },
-    { id: '3', title: '3', content: <div className="size-5 bg-amber-300"></div> },
-    { id: '4', title: '4', content: <div className="size-5 bg-amber-300"></div> },
+    { id: '1', title: '1', content: <p>1</p>},
+    { id: '2', title: '2', content: <p>2</p>},
+    { id: '3', title: '3', content: <p>3</p>},
+    { id: '4', title: '4', content: <p>4</p>},
 ]
+
 let id = 5
 
 function WindowManager() {
@@ -25,24 +25,20 @@ function WindowManager() {
 
     const containerRef = useRef<HTMLDivElement>(null)
 
-    useEffect(() => utils.dynamicSwapy(swapyRef.current, items, 'id', slotItemMap, setSlotItemMap), [items, slotItemMap])
+    useEffect(() => utils.dynamicSwapy(swapyRef.current, items, 'id', slotItemMap, setSlotItemMap), [items]) // [items] // dont put [items, slotItemMap]
 
     useEffect(() => {
         swapyRef.current = createSwapy(containerRef.current!, {
             manualSwap: true,
-            // animation: 'dynamic'
-            // autoScrollOnDrag: true,
-            // swapMode: 'drop',
-            // enabled: true,
-            // dragAxis: 'x',
-            // dragOnHold: true
         })
 
         swapyRef.current.onSwap((event) => {
             setSlotItemMap(event.newSlotItemMap.asArray)
         })
 
-        return () => swapyRef.current?.destroy()
+        return () => {
+            swapyRef.current?.destroy()
+        }
     }, [])
 
     const handleClose = (id: string) => {
@@ -50,21 +46,43 @@ function WindowManager() {
     }
 
     return (
-        <div className="container grid grid-cols-2 grid-rows-2 gap-2 p-2" ref={containerRef}>
+        <div className="max-w-[800px] w-full flex flex-col gap-2 mx-auto" ref={containerRef}>
+            <div className="grid grid-cols-[2fr_1fr] md:grid-cols-[1fr_1fr] gap-2">
                 {slottedItems.map(({ slotId, itemId, item }) => (
-                    <div className="slot" key={slotId} data-swapy-slot={slotId}>
+                    <div
+                        className="rounded-xl h-[100px] data-[swapy-highlighted]:bg-white/20"
+                        key={slotId}
+                        data-swapy-slot={slotId}
+                    >
                         {item &&
-                            <Window id={itemId} title={item.title} onClose={handleClose}>
-                                {item.content ?? <p>...</p>}
-                            </Window>
+                            <div
+                                className="relative flex flex-col items-center justify-center bg-indigo-700 px-0 py-5 rounded-xl w-full h-full"
+                                data-swapy-item={itemId}
+                                key={itemId}
+                            >
+                                <span>{item.title}</span>
+                                <span
+                                    className="absolute right-5 top-2.5 h-[38px] w-[38px] rounded-full bg-[hsl(244.5,57.9%,45.6%)] bg-[url('./delete.svg')] bg-[length:22px_22px] bg-center bg-no-repeat cursor-pointer hover:bg-[hsl(244.5,57.9%,40.6%)]"
+                                    data-swapy-no-drag
+                                    onClick={() => {
+                                        handleClose()
+                                    }}
+                                ></span>
+                            </div>
                         }
                     </div>
                 ))}
-            <div className="item item--add" onClick={() => {
-                const newItem: Item = { id: `${id}`, title: `${id}`}
-                setItems([...items, newItem])
-                id++
-            }}>+</div>
+            </div>
+            <div
+                className="flex items-center justify-center border-4 border-indigo-700 rounded-xl cursor-pointer select-none hover:bg-indigo-700/80"
+                onClick={() => {
+                    const newItem: Item = { id: `${id}`, title: `${id}` }
+                    setItems([...items, newItem])
+                    id++
+                }}
+            >
+                +
+            </div>
         </div>
     )
 }
